774fbc7e2fd630f1bc75fbc7ff77d7b367ca3ba6
Oluwafemi Awoyemi <femikola23@gmail.com>
2022-03-22 13:52:40 +0400

    setup login page

--- db/migrate/20220322091134_add_devise_to_users.rb
+++ db/migrate/20220322091134_add_devise_to_users.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
-
-class DeviseCreateUsers < ActiveRecord::Migration[7.0]
-  def change
-    create_table :users do |t|
+class AddDeviseToUsers < ActiveRecord::Migration[7.0]
+  def self.up
+    change_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -33,12 +31,16 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.datetime :locked_at
 
 
-      t.timestamps null: false
+      # t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
+  end
+
+  def self.down
+    raise ActiveRecord::IrreversibleMigration
   end
 end
