537c8efaaed8ab702d42df930a745f1a317e4423
aakbarkhan <aakbarkhan@gmail.com>
2022-03-23 13:59:33 +0530

    add the food-list page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   # get 'users/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "users#index"
+  root to: "users/sign_in#index"
+
+  devise_scope  :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :foods, only: [:index, :create, :new, :destroy]
 end
