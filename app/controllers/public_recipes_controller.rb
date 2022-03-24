04caab56f39513a55f7169d1e30c2f4d50b78d1e
aakbarkhan <aakbarkhan@gmail.com>
2022-03-24 13:48:52 +0530

    add the total item and food item

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,9 +1,10 @@
 class PublicRecipesController < ApplicationController
   def index
-  
+    @user = current_user
+    @total_item = current_user.recipes.all.count
+    # @total_val = @user.recipe_foods.all(:quantity)
+    # @total_item = current_user.foods.all.count
+    @prices = current_user.foods.all.sum(:price)
     @public_recipes = Recipe.where(public: true)
-  end
-  def total_items
-    @total_item = Food.count
   end
 end
