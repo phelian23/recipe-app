1b99ba42f15f0bb8c3abab2cd7b9e5eeee656f0e
Oluwafemi Awoyemi <femikola23@gmail.com>
2022-03-26 18:46:44 +0400

    Update app

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -1,7 +1,6 @@
 class GeneralShoppingListsController < ApplicationController
   def index
-    @recipe_food = RecipeFood.all
-    @inventory_food = InventoryFood.all
-    @missing_food = @recipe_food.food
+    @missing_food = RecipeFood.all
+    @amount = RecipeFood.all.count
   end
 end
