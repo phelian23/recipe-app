af457cde76e271d4db015cf11b32d0733c556b78
aakbarkhan <aakbarkhan@gmail.com>
2022-03-26 20:22:10 +0530

    Fixed linters

--- test/controllers/inventory_foods_controller_test.rb
+++ test/controllers/inventory_foods_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class InventoryFoodsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get inventory_foods_index_url
     assert_response :success
   end
